503abe554c8670fe3ed05d9ecaba817968ae9fd0
joe-sharp <joe.sharp@procore.com>
2021-01-24 04:04:17 -0600

    Rubocop autocorrect

--- neofetch/quit_calc.rb
+++ neofetch/quit_calc.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'date'
 
-QUIT_DATE_FILE='/Users/joesharp/.config/neofetch/quit_date.txt'
+QUIT_DATE_FILE = '/Users/joesharp/.config/neofetch/quit_date.txt'
 JUUL_COST = 6
 CIG_COST = 8
 delta = 0
@@ -14,13 +16,11 @@ end
 
 def add_commas(num_string)
   num_string = num_string.to_s if num_string.is_a?(Integer)
-  num_string.reverse.scan(/\d{3}|.+/).join(",").reverse
+  num_string.reverse.scan(/\d{3}|.+/).join(',').reverse
 end
 
 juul_total = add_commas(delta * JUUL_COST)
 cig_total = add_commas(delta * CIG_COST)
 
-
 puts "It has been #{delta} days since you quit smoking!"
 puts "You have saved between $#{juul_total} and $#{cig_total} since you quit!"
-
