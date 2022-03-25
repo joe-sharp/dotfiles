0e75060be1d1d286c328e9327b8c1998a760cd94
joe-sharp <joe.sharp@procore.com>
2022-03-25 11:09:58 -0500

    Ensure folder is correct regardless of repo location

--- scripts/configure/iterm.sh
+++ scripts/configure/iterm.sh
@@ -4,7 +4,7 @@ if eval "$(defaults read com.googlecode.iterm2.plist PrefsCustomFolder &> /dev/n
   echo 'iTerm2 appears to already be configured...'
 else
   echo 'Configuring iTerm2...'
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/dev/dotfiles/iterm"
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$PWD/iterm"
   defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
   echo 'Copying font...'
   cp Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf ~/Library/Fonts/
